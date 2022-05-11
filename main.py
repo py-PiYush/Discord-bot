42442371130a288f96d89f0aab6c975fa941728b
PiYush <68802225+py-PiYush@users.noreply.github.com>
2022-05-11 14:53:03 +0000

    Set up bot

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import os
 import discord
+import requests
+import json
+import random
+from replit import db
+
 client = discord.Client()
+
+# Bot will send cheerful message if anyone sends
+# message including a word from following
+sad_words = set(['sad', 'depressed', 'unhappy', 'angry', 'miserable', 'failure', 'fail', 'depressing', 'bad', 'awful'])
+
+starter_encouragements=['Cheer up!', 'Hang in there.', 'You are a great person!']
+
+def get_quote():
+  '''
+  Return a quote from the API
+  '''
+  response = requests.get('https://zenquotes.io/api/random')
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+  return quote
+
+
 
 @client.event
 async def on_ready():
@@ -10,7 +33,18 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  msg = message.content
 
-client.run()
+  if msg.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
+
+my_secret = os.environ['TOKEN']
+client.run(my_secret)
+
+
+
+
